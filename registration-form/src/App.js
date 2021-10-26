8ee45817384c390318c23fca184652f163e734ee
anil <anilyildirim3pc@gmail.com>
2021-10-26 17:17:18 +0200

    feat: add header component and apply basic scss mixins

--- registration-form/src/App.js
+++ registration-form/src/App.js
@@ -1,10 +1,11 @@
 import './App.scss';
 import Form from './components/Form';
-
+import Header
+ from './components/Header';
 function App() {
   return (
     <div className="App">
-      <h1>Registration</h1>
+      <Header titleText="Registration" />
       <Form />
     </div>
   );
---
+++ registration-form/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = ({ titleText }) => {
+
+  return (
+    <header className="header">
+      <h1>{titleText}</h1>
+    </header>
+  );
+};
+
+export default Header;
